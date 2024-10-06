41ba4ccda4296824666d31f16ae7512eae4369b8
SettingDust <settingdust@gmail.com>
2024-10-06 18:12:40 +0800

    fix: split the entrypoints to work on server side

--- src/main/kotlin/settingdust/moreprofiling/Entrypoint.kt
+++ src/main/kotlin/settingdust/moreprofiling/Entrypoint.kt
@@ -1,10 +1,13 @@
 package settingdust.moreprofiling
 
+import net.fabricmc.api.ClientModInitializer
 import net.fabricmc.api.EnvType
+import net.fabricmc.api.ModInitializer
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents
 import net.fabricmc.loader.api.FabricLoader
+import net.fabricmc.loader.api.entrypoint.PreLaunchEntrypoint
 import net.minecraft.SharedConstants
 import net.minecraft.server.dedicated.DedicatedServer
 import net.minecraft.util.profiling.jfr.FlightProfiler
@@ -17,33 +20,42 @@ object MoreProfiling {
     val LOGGER = LogManager.getLogger()!!
 }
 
-fun preLaunch() {
-    SharedConstants.createGameVersion()
-    if (MoreProfilingConfig.common.launchProfiling) {
-        launchProfiling = true
-        FlightProfiler.INSTANCE.start(
-            when (FabricLoader.getInstance().environmentType!!) {
-                EnvType.CLIENT -> InstanceType.CLIENT
-                EnvType.SERVER -> InstanceType.SERVER
-            })
+internal object PreLaunchEntrypoint : PreLaunchEntrypoint {
+    override fun onPreLaunch() {
+        SharedConstants.createGameVersion()
+        if (MoreProfilingConfig.common.launchProfiling) {
+            launchProfiling = true
+            FlightProfiler.INSTANCE.start(
+                when (FabricLoader.getInstance().environmentType!!) {
+                    EnvType.CLIENT -> InstanceType.CLIENT
+                    EnvType.SERVER -> InstanceType.SERVER
+                },
+            )
+        }
     }
 }
 
-fun init() {
-    ServerLifecycleEvents.SERVER_STARTED.register { finishLaunchProfiling() }
+internal object MainEntrypoint : ModInitializer {
+    override fun onInitialize() {
+        ServerLifecycleEvents.SERVER_STARTED.register { finishLaunchProfiling() }
 
-    ClientTickEvents.START_CLIENT_TICK.register {
-        if (it.overlay == null) {
-            finishLaunchProfiling()
+        ServerLifecycleEvents.SERVER_STOPPED.register {
+            if (it is DedicatedServer && FlightProfiler.INSTANCE.isProfiling)
+                FlightProfiler.INSTANCE.stop()
         }
     }
+}
 
-    ServerLifecycleEvents.SERVER_STOPPED.register {
-        if (it is DedicatedServer && FlightProfiler.INSTANCE.isProfiling)
-            FlightProfiler.INSTANCE.stop()
-    }
+internal object ClientEntrypoint : ClientModInitializer {
+    override fun onInitializeClient() {
+        ClientTickEvents.START_CLIENT_TICK.register {
+            if (it.overlay == null) {
+                finishLaunchProfiling()
+            }
+        }
 
-    ClientLifecycleEvents.CLIENT_STOPPING.register {
-        if (FlightProfiler.INSTANCE.isProfiling) FlightProfiler.INSTANCE.stop()
+        ClientLifecycleEvents.CLIENT_STOPPING.register {
+            if (FlightProfiler.INSTANCE.isProfiling) FlightProfiler.INSTANCE.stop()
+        }
     }
 }
