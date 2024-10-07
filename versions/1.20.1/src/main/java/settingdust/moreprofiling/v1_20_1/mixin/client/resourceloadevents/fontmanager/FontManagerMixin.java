731da5a2e95d2accbd97fc28b8687606614f9339
SettingDust <settingdust@gmail.com>
2024-10-07 19:23:26 +0800

    fix: missing `FontManagerMixin` on 1.20.1

---
+++ versions/1.20.1/src/main/java/settingdust/moreprofiling/v1_20_1/mixin/client/resourceloadevents/fontmanager/FontManagerMixin.java
@@ -0,0 +1,33 @@
+package settingdust.moreprofiling.v1_20_1.mixin.client.resourceloadevents.fontmanager;
+
+import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.sugar.Share;
+import com.llamalad7.mixinextras.sugar.ref.LocalRef;
+import net.minecraft.client.font.FontManager;
+import net.minecraft.util.Identifier;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import settingdust.moreprofiling.FontManagerLoadEvent;
+
+@Mixin(FontManager.class)
+public class FontManagerMixin {
+    @Inject(method = "method_51607", at = @At("HEAD"))
+    private void moreprofiling$startEvent(
+        final CallbackInfo ci,
+        @Share("event") LocalRef<FontManagerLoadEvent> eventRef,
+        @Local(argsOnly = true) Identifier id
+    ) {
+        var event = new FontManagerLoadEvent(id.toString());
+        eventRef.set(event);
+        event.begin();
+    }
+
+    @Inject(method = "method_51607", at = @At("TAIL"))
+    private void moreprofiling$stopEvent(
+        final CallbackInfo ci, @Share("event") LocalRef<FontManagerLoadEvent> eventRef
+    ) {
+        eventRef.get().commit();
+    }
+}
