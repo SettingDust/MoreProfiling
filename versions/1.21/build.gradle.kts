9afbb954b67b6af07f5f4ddcee98a78bde865aba
SettingDust <settingdust@gmail.com>
2024-11-06 16:03:34 +0800

    build: remove javadoc

--- versions/1.21/build.gradle.kts
+++ versions/1.21/build.gradle.kts
@@ -8,19 +8,10 @@ val description: String by rootProject.properties
 java {
     toolchain { languageVersion = JavaLanguageVersion.of(21) }
 
-    // Still required by IDEs such as Eclipse and Visual Studio Code
     sourceCompatibility = JavaVersion.VERSION_21
     targetCompatibility = JavaVersion.VERSION_21
 
-    // Loom will automatically attach sourcesJar to a RemapSourcesJar task and to the "build"
-    // task if it is present.
-    // If you remove this line, sources will not be generated.
     withSourcesJar()
-
-    // If this mod is going to be a library, then it should also generate Javadocs in order to
-    // aid with development.
-    // Uncomment this line to generate them.
-    withJavadocJar()
 }
 
 loom {
